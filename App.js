675c5a049e78de8bdc1eb103c4b07ddeb66057e1
Jessica Xiang <jessicaxiang1997@gmail.com>
2017-11-11 22:36:26 -0500

    attempt to add firebase

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, View, Button, Alert, DatePickerIOS, TabBarIOS } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, Alert, DatePickerIOS, TabBarIOS, } from 'react-native';
 
 class DatePickerExample extends React.Component {
 
